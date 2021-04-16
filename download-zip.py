d56161b1ce0f902c97da585ee86854615105cf51
ohway4death <bigplant9411@chiba-u.jp>
2021-04-16 12:29:48 +0900

    first commit

---
+++ download-zip.py
@@ -0,0 +1,18 @@
+import urllib.request
+import urllib.parse
+
+API = "https://api.aoikujira.com/zip/xml/get.php"
+
+values = {
+    'fmt': 'xml',
+    'zn':'2600033'
+}
+params = urllib.parse.urlencode(values)
+
+url = API + "?" + params
+print("url= ", url)
+
+data = urllib.request.urlopen(url).read()
+text = data.decode("utf-8")
+print(text)
+
